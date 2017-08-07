0d8fdebb43e679d78a97116aa6be4af8c2de3662
Alexander <alexander.demidovich.v@gmail.com>
2017-08-07 20:33:21 +0300

    Update script.js
    refactoring

--- script.js
+++ script.js
@@ -1,86 +1,89 @@
-class Input_parameters {
-	constructor(text, color, marker){
-		this.text = text;
-		this.color = color;
-		this.marker = marker;
-	}
-}
+(function()
+{
 
-$(document).ready(function(){
+	var input_parameters = new function() {
+		
+		this.update_fields = function(text, color, marker) {
+			this.text = text;
+			this.color = color;
+			this.marker = marker;
+		}
+	}
 
 	$("#button_create_list_item").on("click", add_list_item);
 	$("#button_delete_list_item").on("click", delete_list_item);
 	$("#button_change_list_item").on("click", change_list_item);
 
 	$("ul").on("click", mark_selected);
-});
 
-function mark_selected(event){
-	remove_selected_class_list();
+	function mark_selected(event){
+		remove_selected_class_list();
 
-	if ( $(event.target).is("li")){
-		$(event.target).addClass("li-selected");
+		if ( $(event.target).is("li")){
+			$(event.target).addClass("li-selected");
 
-		var input_parameters = new Input_parameters($(event.target).text(), 
-			$(event.target).css("color"), 
-			$(event.target).css("list-style-type"));
+			input_parameters.update_fields($(event.target).text(), 
+				$(event.target).css("color"), 
+				$(event.target).css("list-style-type"))
 
-		update_input_fields(input_parameters);
+			update_input_fields(input_parameters);
+		}
+		
+	}
+
+	function update_input_fields(input_parameters){
+		$("#input_text").val(input_parameters.text);
+		$("#input_color").val(input_parameters.color);
+		$("#input_marker").val(input_parameters.marker);
+	}
+
+	function remove_selected_class_list(){
+		$("li.li-selected").removeClass("li-selected");
+	}
+
+	function delete_list_item(event){
+		if(is_selected_item_exist())
+		{
+			$(".li-selected").off("click", mark_selected);
+			$(".li-selected").remove();
+		}
+		else{
+			alert("Please select list item.");
+		}
+	}
+
+	function is_selected_item_exist(){
+		return $('.li-selected').length;
+	}
+
+	function add_list_item(event){
+		event.preventDefault();
+
+		input_parameters.update_fields($("#input_text").val(), 
+				$("#input_color").val(), 
+				$("#input_marker").val());
+
+		$('#list').append('<li>'+input_parameters.text+'</li>');
+		$("li:last")
+			.css("color", input_parameters.color)
+			.css("list-style-type", input_parameters.marker);
+	}
+
+	function change_list_item(event){
+		if(is_selected_item_exist())
+		{
+			input_parameters.update_fields($("#input_text").val(), 
+				$("#input_color").val(), 
+				$("#input_marker").val());
+
+			$(".li-selected")
+				.css("color", input_parameters.color)
+				.css("list-style-type", input_parameters.marker)
+				.text(input_parameters.text);
+		}
+		else{
+			alert("Please select list item.");
+		}
 	}
 	
-}
-
-function update_input_fields(input_parameters){
-	$("#input_text").val(input_parameters.text);
-	$("#input_color").val(input_parameters.color);
-	$("#input_marker").val(input_parameters.marker);
-}
-
-function remove_selected_class_list(){
-	$("li.li-selected").removeClass("li-selected");
-}
-
-function delete_list_item(event){
-	if(is_selected_item_exist())
-	{
-		$(".li-selected").off("click", mark_selected);
-		$(".li-selected").remove();
-	}
-	else{
-		alert("Please select list item.");
-	}
-}
-
-function is_selected_item_exist(){
-	return $('.li-selected').length;
-}
-
-function add_list_item(event){
-	event.preventDefault();
-
-	var input_parameters = new Input_parameters($("#input_text").val(), 
-			$("#input_color").val(), 
-			$("#input_marker").val());
-
-	$('#list').append('<li>'+input_parameters.text+'</li>');
-	$("li:last")
-		.css("color", input_parameters.color)
-		.css("list-style-type", input_parameters.marker);
-}
-
-function change_list_item(event){
-	if(is_selected_item_exist())
-	{
-		var input_parameters = new Input_parameters($("#input_text").val(), 
-			$("#input_color").val(), 
-			$("#input_marker").val());
-
-		$(".li-selected")
-			.css("color", input_parameters.color)
-			.css("list-style-type", input_parameters.marker)
-			.text(input_parameters.text);
-	}
-	else{
-		alert("Please select list item.");
-	}
-}
+})();
